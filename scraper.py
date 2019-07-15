a2b4af9e6dd590af9778496b2995fd0f5063638e
Marc Diaz <marcgabe15@gmail.com>
2019-07-14 22:52:09 -0400

    added user input

--- scraper.py
+++ scraper.py
@@ -1,12 +1,14 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-my_url = {{website name}}
+
+my_url = input("Enter website url")
 
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
 
 page_soup = soup(page_html, "html.parser")
+print(page_soup)
 
 
