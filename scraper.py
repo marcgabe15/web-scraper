bdab7a3e61be13b0d9cad56b45699fca6a45be95
Marc Diaz <marcgabe15@gmail.com>
2019-08-07 00:22:19 -0400

    added git ignore

--- scraper.py
+++ scraper.py
@@ -1,21 +1,21 @@
-import scrapy
-class BricketSpider(scrapy.Spider):
-    name="brickset_spider"
-    start_urls = ['http://brickset.com/sets/year-2016']
-
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
-
+import requests
+from bs4 import BeautifulSoup
 
 my_url = input("Enter website url")
 
-uClient = uReq(my_url)
-page_html = uClient.read()
-uClient.close()
+page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
 
-page_soup = soup(page_html, "html.parser")
-print(page_soup)
+soup = BeautifulSoup(page.text, 'html.parser')
 
 
 
+# Pull all text from the BodyText div
+artist_name_list = soup.find(class_='BodyText')
 
+# Pull text from all instances of <a> tag within BodyText div
+artist_name_list_items = artist_name_list.find_all('a')
+
+for artist_name in artist_name_list_items:
+    print(artist_name.prettify())
+
+        
