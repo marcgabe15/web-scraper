e9b668309e2788d377be2bee0072c960b686132e
Marc Diaz <marcgabe15@gmail.com>
2019-08-18 19:49:52 -0400

    added another sample scraper

--- sample_scraper/scraper.py
+++ sample_scraper/scraper.py
@@ -1,25 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+# my_url = input("Enter website url")
 
-my_url = input("Enter website url")
+pages = []
+for i in range(1, 5):
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + str(i) + '.htm'
+    pages.append(url)
 
-page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+for item in pages:
+    page = requests.get(item)
 
-soup = BeautifulSoup(page.text, 'html.parser')
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-#remove bottom links
-last_links = soup.find(class_='AlphaNav')
-last_links.decompose()
-
-
-# Pull all text from the BodyText div
-artist_name_list = soup.find(class_='BodyText')
+    #remove bottom links
+    last_links = soup.find(class_='AlphaNav')
+    last_links.decompose()
 
 
-# Pull text from all instances of <a> tag within BodyText div
-artist_name_list_items = artist_name_list.find_all('a')
+    # Pull all text from the BodyText div
+    artist_name_list = soup.find(class_='BodyText')
 
-for artist_name in artist_name_list_items:
-    print(artist_name.prettify())
+
+    # Pull text from all instances of <a> tag within BodyText div
+    artist_name_list_items = artist_name_list.find_all('a')
+
+    f = csv.writer(open('z-artist-names.csv', 'w'))
+    f.writerow(['Name', 'Link'])
+
+    for artist_name in artist_name_list_items:
+        names = artist_name.contents[0]
+        links = 'https://web.archive.org' + artist_name.get('href')
+        print(names)
+        print(links)
+        f.writerow([names, links])
+
 
 
