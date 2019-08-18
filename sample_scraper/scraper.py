d23a170d7bc553666366aef6084dadbdb0d5fcd6
Marc Diaz <marcgabe15@gmail.com>
2019-08-18 18:15:22 -0400

    added diff folder

---
+++ sample_scraper/scraper.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+my_url = input("Enter website url")
+
+page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+#remove bottom links
+last_links = soup.find(class_='AlphaNav')
+last_links.decompose()
+
+
+# Pull all text from the BodyText div
+artist_name_list = soup.find(class_='BodyText')
+
+
+# Pull text from all instances of <a> tag within BodyText div
+artist_name_list_items = artist_name_list.find_all('a')
+
+for artist_name in artist_name_list_items:
+    print(artist_name.prettify())
+
+
